be97671f9192ba41e8d459479acd109ce76fb604
Gervais BERI <beriaxel@gmail.com>
2024-10-07 21:30:31 +0000

    Created Footer.jsx

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import "./EventPlanner.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer>
+        <p>&copy; 2024 Event Planner Organization. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
